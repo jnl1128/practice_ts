919affb781b89c75de74df4ad9a271d843fb82d1
narin <nalynnshinhwa1128@gmail.com>
2023-03-23 09:33:17 +0900

    ts와 style-components 함께 사용하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    background-color: ${props => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+    color: ${props => props.theme.textColor};
+`;
 
 function App() {
-    const [value, setValue] = useState("");
-    // 타입스크립트에게 어떤 종류의 element가 onChange 이벤트를 발생시키는지 명시하기
-    // 아래의 코드에서는 input element에 의해 onChange 이벤트가 발생함
-    const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-        const {
-            currentTarget: { value },
-        } = event;
-        setValue(value);
-    };
-    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log("hello", value);
-    };
     return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <input onChange={onChange} value={value} type="text" placeholder="username" />
-                <button>Login</button>
-            </form>
-        </div>
+        <Container>
+            <H1>protected</H1>
+        </Container>
     );
 }
 
